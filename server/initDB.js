6195c715d9b7052da2feff37a9246ad78650fc1a
raymondly <rla65@sfu.ca>
2023-07-11 09:54:52 -0700

    Fix initDB.js; temporarily disable protected routes

--- server/initDB.js
+++ server/initDB.js
@@ -3,14 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const client = new pg.Client({
+  user: process.env.PG_USER,
   password: process.env.PG_PASSWORD
 })
 
 // Create DB
-await client.connect({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-});
+await client.connect();
 await client.query("CREATE DATABASE dateplanner");
 await client.end();
 
