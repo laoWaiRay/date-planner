014bbb23c3b402e1b82866d4216760d79e2b5c71
gssethi <gssethi@sfu.ca>
2023-07-22 03:18:03 -0700

    added a polaroid component for memories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,28 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import useLogout from "../hooks/useLogout";
 import "./Home.css";
 
-import InvitationCard from '../components/InvitationCard';
-import UpcomingCard from '../components/UpcomingCard';
+import InvitationCard from "../components/InvitationCard";
+import UpcomingCard from "../components/UpcomingCard";
+import Polaroid from "../components/Polaroid";
 
 export default function Home() {
   const { user } = useAuthContext();
 
   const logout = useLogout();
 
-  const backgroundImage = "src/assets/Hero.jpg"; // Update image path
-  const avatarImage = "src/assets/avatar.png"; // Update image path
+  const backgroundImage = "src/assets/Hero.jpg";
+  const avatarImage = "src/assets/avatar.png";
   const username = "Gurpreet Sethi";
 
   return (
     <>
       <div className="homepage-container">
+        <div style={{ marginLeft: "100px" }}>
+          {" "}
+          <Polaroid imageSrc={backgroundImage} caption={username} />
+        </div>
+      </div>
+      {/* <div className="homepage-container">
         <div className="background-image-container">
           <img
             src={backgroundImage}
@@ -32,16 +39,19 @@ export default function Home() {
         </div>
         <div className="floating-card-invitations">
           <h2 className="text-center">INVITATIONS</h2>
-          <InvitationCard/>
-          <InvitationCard/>
+          <InvitationCard />
+          <InvitationCard />
         </div>
         <div className="floating-card-upcoming">
           <h2 className="text-center">UPCOMING DATES</h2>
           <UpcomingCard />
-           <UpcomingCard />
-            <UpcomingCard />
+          <UpcomingCard />
+          <UpcomingCard />
         </div>
-      </div>
+        <div className="floating-card-polariod">
+          
+        </div>
+      </div> */}
     </>
   );
 }
